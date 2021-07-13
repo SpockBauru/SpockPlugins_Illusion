0dd3dc73da06133683db4ab6e7ad1e5314ae316d
SpockBauru <67930504+SpockBauru@users.noreply.github.com>
2021-07-13 20:16:22 -0300

    First working version

--- HS2_GirlsEntrance/HS2_GirlsEntrance.cs
+++ HS2_GirlsEntrance/HS2_GirlsEntrance.cs
@@ -1,4 +1,6 @@
-﻿using HarmonyLib;
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
 using UnityEngine;
 using System;
 using UnityEngine.UI;
@@ -6,8 +8,6 @@ using HS2;
 using Manager;
 using SceneAssist;
 using Illusion.Game;
-//using UniRx;
-//using UniRx.Triggers;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
@@ -22,54 +22,98 @@ using Illusion.Extensions;
 using GameLoadCharaFileSystem;
 using UIAnimatorCore;
 using UnityEngine.EventSystems;
-using BepInEx;
+using System.ComponentModel;
+using UniRx;
 
 namespace HS2_GirlsEntrance
 {
     [BepInProcess("HoneySelect2")]
-    [BepInPlugin("SpockBauru.hs2.GirlsEntrance", "HS2_GirlsEntrance", Version)]
+    [BepInPlugin("spockbauru.hs2.girlsentrance", "HS2_GirlsEntrance", Version)]
     public class HS2_GirlsEntrance : BaseUnityPlugin
     {
-        public const string Version = "0.1";
+        //Set versuon in BepInEx and in AssemblyInfo
+        public const string Version = "0.2";
+
+        //User Configurations
+        private static ConfigEntry<bool> Enabled;
+        private static ConfigEntry<EntranceOption> MakeEntrance;
+        private static ConfigEntry<WherePlayAnimation> WherePlay;
+
+        //User can define girl's state to play animation
+        private enum EntranceOption
+        {
+            [Description("On Girl's First Time")]
+            FirstTime,
+            [Description("Every Time")]
+            EveryTime
+        }
+
+        //User can define in which place he wants to play the animation
+        private enum WherePlayAnimation
+        {
+            [Description("Before Map Select")]
+            MapSelect,
+            [Description("When Select a Girl")]
+            GirlSelect
+        }
+
         public HS2_GirlsEntrance()
         {
+            //Patch Everything
             Harmony.CreateAndPatchAll(typeof(HS2_GirlsEntrance));
+
+            //Config Panel Settings
+            Enabled = Config.Bind("General", "Enabled", true, "Whether the plugin is enabled");
+            MakeEntrance = Config.Bind("Settings", "When make an entrance", EntranceOption.FirstTime, "When the entrance animation plays");
+            WherePlay = Config.Bind("Settings", "Where play the animation", WherePlayAnimation.MapSelect, "Which place do you want to play the animation");
         }
 
+        //Pay the animation on girls's select
+        [HarmonyPostfix, HarmonyPatch(typeof(LobbyCharaSelectInfoScrollController), "OnValueChange")]
+        public static void SetupGirlsEntrance()
+        {
+            if (Enabled.Value && WherePlay.Value == WherePlayAnimation.GirlSelect)
+            {
+                EnterAnimation();
+            }
+
+        }
+
+        //Play the animation on map select
         [HarmonyPrefix, HarmonyPatch(typeof(LobbyMapSelectUI), "InitList")]
-        public static void LobbyFashion()
+        public static void LoadOnMapSelect()
         {
-            Console.WriteLine("============AAAAAAAAAAAAAAAA=================");
-
-            //Instance of LobbySceneManager is needed for all things. I coudn't open an existing one
-            LobbySceneManager lm_mod = Singleton<LobbySceneManager>.Instance;
-                        
-            Console.WriteLine(lm_mod.heroines[0].gameinfo2.hCount);
-            // How many times the girl had sex
-            int sexTimes = lm_mod.heroines[0].gameinfo2.hCount;
-
-            // If virgin, play animation
-            if (sexTimes == 0)
-                EnterAnimation(lm_mod);
+            if (Enabled.Value && WherePlay.Value == WherePlayAnimation.MapSelect)
+            {
+                EnterAnimation();
+            }
         }
 
-        public static void EnterAnimation(LobbySceneManager lm_mod)
+        //Calls the animation
+        public static void EnterAnimation()
         {
-            Console.WriteLine("============BBBBBBBBBBBBBBBB=================");
-            //Set ADV file to load
-            string bundle_mod = "adv/scenario/op/30/04.unity3d";
+            //Instance of LobbySceneManager
+            LobbySceneManager scene = Singleton<LobbySceneManager>.Instance;
+            int sexTimes = scene.heroines[0].gameinfo2.hCount;
 
-            //Set Name/PathID inside the file
-            string asset_mod = "0";
+            if (MakeEntrance.Value == EntranceOption.FirstTime && sexTimes == 0 ||
+                MakeEntrance.Value == EntranceOption.EveryTime)
+            {
+                //Set ADV file to load
+                string bundle = "adv/scenario/op/50/entrance.unity3d";
 
-            //Set first Girl
-            Heroine herone_mod = lm_mod.heroines[0];
+                //Set Name/PathID inside the file
+                string asset = "0";
 
-            //no idea
-            Action onEnd_mod = null;
+                //Set first Girl
+                Heroine heroine = scene.heroines[0];
 
-            //Open the scene
-            lm_mod.OpenADV(bundle_mod, asset_mod, herone_mod, onEnd_mod);
+                //No idea
+                Action onEnd = null;
+
+                //Open the scene
+                scene.OpenADV(bundle, asset, heroine, onEnd);
+            }
         }
     }
 }
