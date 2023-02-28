18285e82c9d96977fe691bec486a4e20bd6f3ee4
SpockBauru <67930504+SpockBauru@users.noreply.github.com>
2023-02-28 16:50:26 -0300

    Moved RG_TextDump to TranslationTools_Illusion

--- RG_TextDump/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using IllusionPlugins;
-using System.Reflection;
-using static IllusionPlugins.RG_TextDump;
-
-[assembly: AssemblyTitle(PluginName)]
-[assembly: AssemblyProduct(PluginName)]
-[assembly: AssemblyDescription(PluginName + " for " + Constants.GameName)]
-[assembly: AssemblyVersion(Version)]
-[assembly: AssemblyFileVersion(Version)]
