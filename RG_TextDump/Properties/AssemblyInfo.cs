5a4a6b861f473f4ed2ecc4187a9212357051b295
SpockBauru <67930504+SpockBauru@users.noreply.github.com>
2023-02-27 23:12:19 -0300

    TextDump prototype

---
+++ RG_TextDump/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using IllusionPlugins;
+using System.Reflection;
+using static IllusionPlugins.RG_TextDump;
+
+[assembly: AssemblyTitle(PluginName)]
+[assembly: AssemblyProduct(PluginName)]
+[assembly: AssemblyDescription(PluginName + " for " + Constants.GameName)]
+[assembly: AssemblyVersion(Version)]
+[assembly: AssemblyFileVersion(Version)]
